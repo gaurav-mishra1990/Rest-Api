ff59c7695996d0f349f0e6cc11ee4386748dd93c
Gaurav Mishra <gaurav@simpragma.com>
2019-12-24 02:45:14 +0530

    Added Insertion method to Postgres Database.

--- apis/Log/dao.py
+++ apis/Log/dao.py
@@ -2,9 +2,41 @@ from elasticsearch import Elasticsearch
 import os
 import json
 
+
 storage_type = os.getenv("STORAGE_TYPE")
 es_host = os.getenv("ES_HOST")
 es_port = os.getenv("ES_PORT")
+
+
+def insert_db(log):
+    from models import Application_Log
+    from app import db
+
+    application_id = log["application_id"]
+    application_log = log["application_log"]
+    timestamp = application_log["timestamp"]
+    log_message = application_log["log_message"]
+
+    keys = application_log.keys()
+
+    logging_mode = application_log["logging_mode"] if "logging_mode" in keys else None
+    host_ip = application_log["host_ip"] if "host_ip" in keys else None
+    access_request = application_log["access_request"] if "access_request" in keys else None
+    result_status_code = application_log["result_status_code"] if "result_status_code" in keys else None
+    user_agent = application_log["user_agent"] if "user_agent" in keys else None
+    user_name = application_log["user_name"] if "user_name" in keys else None
+    bytes_transferred = application_log["bytes_transferred"] if "bytes_transferred" in keys else None
+
+    log_object = Application_Log(application_id=application_id, timestamp=timestamp, log_message=log_message,
+                                logging_mode=logging_mode, host_ip=host_ip, access_request=access_request,
+                                result_status_code=result_status_code, user_agent=user_agent, user_name=user_name,
+                                bytes_transferred=bytes_transferred)
+
+    db.session.add(log_object)
+    db.session.commit()
+    db.session.close()
+
+
 
 
 def connect_elasticsearch():
@@ -14,11 +46,11 @@ def connect_elasticsearch():
         return True
     else:
         return False
-    return _es
 
 
 def insert_log_in_db(log):
     es_connected = True
+    # es_connected = connect_elasticsearch()
     if es_connected:
         if storage_type == 'file_storage':
             log_file = os.getenv("LOG_FILE")
@@ -32,6 +64,10 @@ def insert_log_in_db(log):
                 log_str = json.dumps(log, indent=4)
                 f.write('{},\n'.format(log_str))
                 f.close()
+        elif storage_type == 'database':
+            insert_db(log)
+        else:
+            pass
     else:
         pass
     
--- models.py
+++ models.py
@@ -10,15 +10,27 @@ class Application(db.Model):
 
 class Application_Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    application_id = db.Column(db.Integer, db.ForeignKey(Application.application_id), nullable=False)
+    application_id = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     logging_mode = db.Column(db.String(64), index=True)
     host_ip = db.Column(db.String(64))
     access_request = db.Column(db.String(64), index=True)
-    application_type = db.Column(db.String(64), index=True)
     result_status_code = db.Column(db.Integer, index=True)
     user_agent = db.Column(db.String(64))
     user_name = db.Column(db.String(64))
     bytes_transferred = db.Column(db.BigInteger)
     log_message = db.Column(db.String(250))
+
+    def __init__(self, application_id, timestamp, logging_mode, host_ip, access_request, result_status_code, user_agent, user_name, bytes_transferred, log_message):
+        
+        self.application_id = application_id
+        self.timestamp = timestamp
+        self.logging_mode = logging_mode
+        self.host_ip = host_ip
+        self.access_request = access_request
+        self.result_status_code = result_status_code
+        self.user_agent = user_agent
+        self.user_name = user_name
+        self.bytes_transferred = bytes_transferred
+        self.log_message = log_message
     
